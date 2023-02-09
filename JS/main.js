43840eea7ee089e4145a1caf791447e19b5f7203
Trieu <tranbinhtrieuhgbt2004@gmail.com>
2023-02-09 21:54:11 +0700

    Khoi tao baitap2

--- JS/main.js
+++ JS/main.js
@@ -64,7 +64,71 @@ document.getElementById("btnTinhLuong").onclick = tinhTienLuong;
  */
 
 function tinhGTTB() {
-    
+    var soThuNhat = document.getElementById("thuNhat").value;
+    var soThuHai = document.getElementById("thuHai").value;
+    var soThuBa = document.getElementById("thuBa").value;
+    var soThuBon = document.getElementById("thuBon").value;
+    var soThuNam = document.getElementById("thuNam").value;
+
+    var GTTB = (Number(soThuNhat)+Number(soThuHai)+Number(soThuBa)+Number(soThuBon)+Number(soThuNam)) / 5;
+    console.log(GTTB)
+
+    document.getElementById("ketGtriTB").innerHTML ="Điểm Trung Bình là :"+ GTTB;
+
 }
 
 document.getElementById("btnGtriTB").onclick = tinhGTTB;
+
+
+
+ //! BAITAP3
+/**
+ * Khối 1: input
+ *  tienUSD
+ * 
+ * Khối 2:
+ * 
+ * B1: lấy gtrị từ form khi user click
+ * + tạo hàm tính điểm
+ * + gán hàm vào sự kiện click của button
+ * 
+ * B2: trong hàm 
+ * + lấy gtrị từ form
+ * + lập Giá USD quy đổi ra VNĐ
+ * + hiển thị kết quả lên UI
+ * 
+ * Khối 3:output
+ *  VND
+ * 
+ */
+
+
+function quyDoiTien() {
+    var doiTien = document.getElementById("tienUSD")
+
+}
+
+
+
+
+
+//! BAITAP
+/**
+ * Khối 1: input
+ *  thuNhat , thuHai ,thuBa ,thuBon,thuNam
+ * 
+ * Khối 2:
+ * 
+ * B1: lấy gtrị từ form khi user click
+ * + tạo hàm tính điểm
+ * + gán hàm vào sự kiện click của button
+ * 
+ * B2: trong hàm 
+ * + lấy gtrị từ form
+ * + lập công thức tính gtrị TB
+ * + hiển thị kết quả lên UI
+ * 
+ * Khối 3:output
+ * GTTB
+ * 
+ */
