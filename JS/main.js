1d22351f1f1ddde6cce79d07a2e0c3eb67590380
Trieu <tranbinhtrieuhgbt2004@gmail.com>
2023-02-09 20:21:09 +0700

    Khoi tao

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+
+
+        //! BAITAP1 
+/**
+ * Khối 1: input
+ *  tienLuong , soNgay
+ * 
+ * Khối 2:
+ * 
+ * B1: lấy gtrị từ form khi user click
+ * + tạo hàm tính điểm
+ * + gán hàm vào sự kiện click của button
+ * 
+ * B2: trong hàm 
+ * + lấy gtrị từ form
+ * + lập công thức tính tiền lương
+ * + hiển thị kết quả lên UI
+ * 
+ * Khối 3:output
+ * ttl
+ * 
+ */
+
+function tinhTienLuong(){
+
+    var tienLong = document.getElementById("tienLuong").value;
+    var soNgay = document.getElementById("ngayLam").value ;
+    console.log(tienLong,soNgay);
+
+    var luong = (tienLong * soNgay );
+    console.log(luong);
+
+    document.getElementById("ketQuaTinh").innerHTML = "Số tiền lương là : <span>$</span>" + luong.toLocaleString();
+}
+
+document.getElementById("btnTinhLuong").onclick = tinhTienLuong;
+
+
+
+
+
+
+
+
+ //! BAITAP2 
+/**
+ * Khối 1: input
+ *  tienLuong , soNgay
+ * 
+ * Khối 2:
+ * 
+ * B1: lấy gtrị từ form khi user click
+ * + tạo hàm tính điểm
+ * + gán hàm vào sự kiện click của button
+ * 
+ * B2: trong hàm 
+ * + lấy gtrị từ form
+ * + lập công thức tính tiền lương
+ * + hiển thị kết quả lên UI
+ * 
+ * Khối 3:output
+ * ttl
+ * 
+ */
